65e79c1dcdb8e61d60356584a4a86d54ae96b06a
Alexis Yepez <alexixyepex@gmail.com>
2026-04-06 15:35:22 -0500

    feat: Implement order lifecycle execution with isolation and collision checks

---
+++ src/test/java/com/foodtech/automation/api/screenplay/tasks/orders/ExecuteOrderLifecycleTwice.java
@@ -0,0 +1,49 @@
+package com.foodtech.automation.api.screenplay.tasks.orders;
+
+import com.foodtech.automation.api.screenplay.questions.LastResponseStatus;
+import com.foodtech.automation.api.screenplay.support.context.CrudExecutionContext;
+import com.foodtech.automation.api.screenplay.support.data.OrderTestDataFactory;
+import com.foodtech.automation.api.screenplay.support.model.OrderPayload;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ExecuteOrderLifecycleTwice implements Task {
+
+    public static ExecuteOrderLifecycleTwice forTheCurrentOrder() {
+        return Tasks.instrumented(ExecuteOrderLifecycleTwice.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        String token = CrudExecutionContext.current().token();
+
+        executeLifecycleIteration(actor);
+
+        OrderPayload createPayload = OrderTestDataFactory.createPayload();
+        OrderPayload updatePayload = OrderTestDataFactory.updatePayloadFrom(createPayload);
+        CrudExecutionContext.init(createPayload, updatePayload).setToken(token);
+
+        executeLifecycleIteration(actor);
+    }
+
+    private void executeLifecycleIteration(Actor actor) {
+        actor.attemptsTo(CreateOrder.usingContextPayload());
+        actor.should(seeThat(LastResponseStatus.code(), equalTo(201)));
+
+        actor.attemptsTo(GetOrderById.fromContext());
+        actor.should(seeThat(LastResponseStatus.code(), equalTo(200)));
+
+        actor.attemptsTo(UpdateOrderById.usingContextPayload());
+        actor.should(seeThat(LastResponseStatus.code(), equalTo(200)));
+
+        actor.attemptsTo(DeleteOrderById.fromContext());
+        actor.should(seeThat(LastResponseStatus.code(), equalTo(204)));
+
+        actor.attemptsTo(GetOrderById.fromContext());
+        actor.should(seeThat(LastResponseStatus.code(), equalTo(404)));
+    }
+}
